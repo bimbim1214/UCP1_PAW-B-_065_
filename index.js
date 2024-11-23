6d835fe49a00ab40680c53a15175ac4cf313aa04
bimbim1214 <bimoadityapangestu@gmail.com>
2024-11-23 20:31:35 +0700

    untuk membuat instance aplikasi Express, yang akan digunakan untuk mengatur rute dan middleware dan tentukan port server pada 9000

--- index.js
+++ index.js
@@ -9,22 +9,3 @@ const PORT = 9000;
 //const express = require('express');
 //const hewanRoutes = require('./routes/hewan.js');
 
-app.use(bodyParser.json());
-app.use(express.json());
-
-// app.use("/hewan", hewanRoutes);
-// app.use("/todo", todoRoutes);
-// app.use('view engine', 'ejs');
-// app.use('/', (req, res) => {
-//     res.render('index');
-// });
-
-app.use("/pupuk", pupukRoutes);
-app.use("/bibit", bibitroute);
-app.get("/", (req, res) => {
-    console.log(["GET ROUTE"]);
-    res.send("Selamat Pagi")
-});
-
-app.use(bodyParser.json());
-app.listen(PORT, () => console.log(`Server berjalan di port : http://localhost:${PORT}`));
