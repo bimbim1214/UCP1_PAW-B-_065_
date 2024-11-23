e36eef445b5fb98c16199ce7866d3bd2834c986e
bimbim1214 <bimoadityapangestu@gmail.com>
2024-11-23 20:24:09 +0700

    implementasi endpoint POST untuk menambahkan data pupuk baru dengan validasi dan pembuatan ID otomatis

--- routes/pupuk.js
+++ routes/pupuk.js
@@ -21,4 +21,17 @@ router.get("/", (req, res) => {
     res.send(pupuk);
 });
 
+router.post('/', (req, res) => { 
+    const newpupuk = { 
+        id: pupuk.length + 1, 
+        namapupuk: req.body.namapupuk,
+        jenispupuk: req.body.jenispupuk,
+        jumlahstok: req.body.jumlahstok, // Menambahkan jumlahstok jika perlu
+        tahun: req.body.tahun
 
+        //completed: false 
+    }; 
+    pupuk.push(newpupuk); 
+    res.status(201).json(newpupuk); 
+}); 
+ 
