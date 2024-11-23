e012326f2f6b88b9e366528d795a3e461bf83145
bimbim1214 <bimoadityapangestu@gmail.com>
2024-11-23 20:03:04 +0700

    menambahkan express router and mock data untuk bibit setup router dengan sample bibit inventory

--- routes/bibit.js
+++ routes/bibit.js
@@ -3,20 +3,17 @@ const router = express.Router();
 
 const bibit = [
     {
+        id: 1,
         namaBibit: "tumbuhan",
         jenisBibit: "kuat",
         JumlahStok: "100",
         tahun: 2024
     },
     {
+        id : 2,
         namaBibit: "batang",
         jenisBibit: "perkasa",
         JumlahStok: "100",
         tahun: 2027
     },
 ];
-router.get("/", (req, res) => {
-    res.send(mobil);
-
-});
-export default router;
